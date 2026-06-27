51692e3d2e54c55b83dbaa0c40f8a8ee51bf8ee7
Uma-0331 <umaumikaa@gmail.com>
2026-06-27 10:35:22 +0530

    Delete recipient_patient directory

--- recipient_patient/patient/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add, name='add'),
-    path('edit/<int:id>/', views.edit, name='edit'),
-    path('delete/<int:id>/', views.delete, name='delete'),
-]
